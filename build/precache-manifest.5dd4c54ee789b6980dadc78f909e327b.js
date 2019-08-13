07c1c1c54cada534f7290f01a697fb043e2edb77
buckeyeburl <tkgwheeler@gmail.com>
2019-08-13 20:05:38 +0100

    Added build folder for front end

---
+++ build/precache-manifest.5dd4c54ee789b6980dadc78f909e327b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ff8ae77e4e1c76b2e6d13f4f9059bd4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd9c68b3256d8b5704ee",
+    "url": "/static/css/main.457cdf3e.chunk.css"
+  },
+  {
+    "revision": "6a19a0beea039a280f26",
+    "url": "/static/js/2.3c62b441.chunk.js"
+  },
+  {
+    "revision": "dd9c68b3256d8b5704ee",
+    "url": "/static/js/main.5764f619.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
